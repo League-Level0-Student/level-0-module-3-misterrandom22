c1a344b444902dda0fe8cddc31f289458472cf0f
404911 <404911@0338S-S-R0B6ZHP.sdcs.local>
2020-02-11 14:24:26 -0800

    F	robot color chooser

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,24 +4,57 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
+		Robot hal = new Robot();
+		// 3. Ask the user what color they would like the robot to draw
+		hal.setX(650);
+		hal.setY(350);
+		for (int i1 = 0; 1 < 5; i1++) {
+			String color = JOptionPane.showInputDialog(null, "What color would you like?");
+			// 5. Use an if/else statement to set the pen color that the user requested
 
-        //6. If the user doesn’t enter anything, choose a random color
+			if(color.equalsIgnoreCase("green")) {
+				hal.setPenColor(Color.green);
+			}
+			else if(color.equalsIgnoreCase("blue")) {
+				hal.setPenColor(Color.blue);
+			}
+			else if(color.equalsIgnoreCase("yellow")) {
+				hal.setPenColor(Color.yellow);
+			}
+			else if(color.equalsIgnoreCase("red")){
+				hal.setPenColor(Color.red);
+			}
+			else{
+				hal.setRandomPenColor();
+			}
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+			hal.penDown();
+			hal.setPenWidth(10);
 
+			hal.hide();
+			hal.penDown();
+			hal.setSpeed(100);
+			int sides = 5;
+			int angle = 360 / sides;
+				hal.move(i1);
+				hal.turn(angle);
+				hal.turn(1);
 
+				// 6. If the user doesn’t enter anything, choose a random color
+			}
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
+
+			// 2. Make the robot draw a shape (this will take more than one line of code)
+
+		}
 	}
-}
+
